b8bde25b6aeabba0d2f2a875381357d27e2e23e4
Anton Nekrasov <anton.nekrasov@softensity.com>
2013-01-09 18:04:21 +0300

    started creating grid

--- js/area/viewport.js
+++ js/area/viewport.js
@@ -1,50 +1,27 @@
-/*globals console*/
+/*globals console, integerDivision*/
 function Viewport () {
     "use strict";
-    // main properties of the tank object
+    
+    //
+    //
+    // properties
+    //
+    //
+    
     var title = 'Viewport',
         description = 'The main viewport',
         height = 0,
-        width = 0;
-
+        width = 0,
+        TOP_SHIFT = 30,
+        LEFT_SHIFT = 30,
+        CELL = 25; 
+    
+    //
+    //
     // main methods of the tank object
-    function init() {
-        var body = document.body,
-            place = document.createElement("DIV");
+    //
+    //
 
-        body.innerHTML = "";
-        width = Math.max(document.documentElement.clientWidth,
-                         document.body.scrollWidth,
-                         document.documentElement.scrollWidth,
-                         document.body.offsetWidth,
-                         document.documentElement.offsetWidth);
-        height = Math.max(document.documentElement.clientHeight,
-                          document.body.scrollHeight,
-                          document.documentElement.scrollHeight,
-                          document.body.offsetHeight,
-                          document.documentElement.offsetHeight);
-        
-        // console.log(width%25);
-        // console.log(height%25);
-        
-        place.setAttribute("id", "area");
-        place.setAttribute("style",
-                // " position: absolute;"+
-                " width: " + (width - 30) + "px;" +
-                " height: " + (height - 30) + "px;" 
-                // " top: 15px;" +
-                // " left: 15px; "
-                );
-
-        document.body.appendChild(place);        
-        
-        
-        
-        
-    }
-    init();
-    
-    
     function getTitle() {
         return title;
     }
@@ -64,6 +41,66 @@ function Viewport () {
     function toString() {
         return 'width: ' + getWidth() + " height: " + getHeight;
     }
+    
+    //supplementary method for easing area design
+    function setGrid(){
+        var grid = document.createElement("TABLE"),
+            rows = integerDivision(height, CELL),
+            columns = integerDivision(width, CELL),
+            r=0,
+            c=0;
+        grid.setAttribute("style", "position: absolute;" + 
+                "top: " + TOP_SHIFT/2 + "px;" + 
+                "left: " + LEFT_SHIFT/2 + "px;" + 
+                "z-index: 20;" 
+        );
+        grid.setAttribute("border", "1");
+        console.log(rows);
+        for (r=0; r < rows; r++) {
+            var tr = document.createElement("TR");
+            for (c=0; c < columns; c++) {
+                var td = document.createElement("TD");
+                td.setAttribute("width", CELL);
+                td.setAttribute("height", CELL);
+                tr.appendChild(td);
+            }
+            grid.appendChild(tr);
+        }
+        document.body.appendChild(grid);
+    }
+    
+    // 
+    //
+    // configuring
+    //
+    //
+    
+    function init() {
+        var body = document.body,
+            place = document.createElement("DIV");
+
+        body.innerHTML = "";
+        width = Math.max(document.documentElement.clientWidth,
+                         document.body.scrollWidth,
+                         document.documentElement.scrollWidth,
+                         document.body.offsetWidth,
+                         document.documentElement.offsetWidth);
+        height = Math.max(document.documentElement.clientHeight,
+                          document.body.scrollHeight,
+                          document.documentElement.scrollHeight,
+                          document.body.offsetHeight,
+                          document.documentElement.offsetHeight);
+        
+        place.setAttribute("id", "area");
+        place.setAttribute("style",
+                " width: " + (width - LEFT_SHIFT) + "px;" +
+                " height: " + (height - TOP_SHIFT) + "px;" + 
+                " z-index: 10;" 
+        );
+        document.body.appendChild(place);        
+    }
+    init();
+    setGrid();
 
     return {
         getTitle: getTitle,
--- js/utils.js
+++ js/utils.js
@@ -1,20 +1,23 @@
 /*globals console*/
-var UTIL = {
-    extend: function(Child, Parent){
-        "use strict";
-        if(!Child || !Parent){
-            console.err("Invalid arguments");
-            return;
-        }
-        var F = function() {
-            console.log("f");
-        };
-        F.prototype = Parent.prototype;
-        Child.prototype = new F();
-        Child.prototype.constructor = Child;
-        Child.superclass = Parent.prototype;
+
+// extends base class
+function extend(Child, Parent){
+    "use strict";
+    if(!Child || !Parent){
+        console.err("Invalid arguments");
+        return;
     }
-    
-    
-    
-};
+    var F = function() {
+        console.log("f");
+    };
+    F.prototype = Parent.prototype;
+    Child.prototype = new F();
+    Child.prototype.constructor = Child;
+    Child.superclass = Parent.prototype;
+}
+
+// integer division
+function integerDivision(x, y){
+    "use strict";
+    return (x-x%y)/y;
+}
